2388159b84ec56a70c619ba3833b8cb3a5721832
Michael Dobrijevic <michael.dobrijevic@gmail.com>
2017-12-06 23:01:25 +1000

    First commit!

---
+++ defaults/defaults.php
@@ -0,0 +1,10 @@
+<?php
+
+
+$page_wide["title"] = "My Portfolio!";
+$page_wide["author"] = "Michael Dobrijevic";
+$page_wide["description"] = "Basic portfolio to show off my experience as a developer";
+$page_wide["styles"] = [
+
+	"static/css/main.css"
+];
---
+++ templates/header.php
@@ -0,0 +1,27 @@
+<?php
+
+include("../defaults/defaults.php");
+?>
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<meta name="description" content="<?php echo $page_wide['description'];?>">
+		<meta name="author" content="<?php echo $page_wide['author']; ?>">
+
+		<?php
+			foreach($page_wide["styles"] as $style) {
+
+				echo "<link rel='stylesheet' type='text/css' href='../$style'>";
+			}
+		?>
+	</head>
+	<body>
+
+			<h1>It works!</h1>
+	</body>
+
+</html>
